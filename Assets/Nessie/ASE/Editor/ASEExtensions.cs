6b07c7c07da34c693f4a87fc36eed8911e21ef52
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2023-12-15 20:35:12 +0100

    Added a class for ASE extension methods

---
+++ Assets/Nessie/ASE/Editor/ASEExtensions.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using AmplifyShaderEditor;
+
+namespace Nessie.ASE.Editor
+{
+    public static class ASEExtensions
+    {
+        public static ParentNode GetActiveNode(DrawInfo drawInfo, List<ParentNode> nodes)
+        {
+            int nodeCount = nodes.Count;
+            for (int i = nodeCount - 1; i >= 0; i--)
+            {
+                ParentNode node = nodes[i];
+                if (!node.IsVisible || node.IsMoving) continue;
+                if (node.GlobalPosition.Contains(drawInfo.MousePosition)) return node;
+            }
+
+            return null;
+        }
+        
+        public static int GetActiveChannels(this ParentNode node)
+        {
+            switch (node.OutputPorts[0].DataType)
+            {
+                case WirePortDataType.FLOAT:
+                    return 1;
+                case WirePortDataType.FLOAT2:
+                    return 2;
+                case WirePortDataType.COLOR:
+                case WirePortDataType.FLOAT4:
+                case WirePortDataType.SAMPLER1D:
+                case WirePortDataType.SAMPLER2D:
+                case WirePortDataType.SAMPLER3D:
+                case WirePortDataType.SAMPLERCUBE:
+                case WirePortDataType.SAMPLER2DARRAY:
+                    return 4;
+                default:
+                    return 3;
+            }
+        }
+
+        public static bool IsPreviewVisible(this ParentNode node)
+        {
+            if (!node.ShowPreview && !node.ContainerGraph.ParentWindow.GlobalPreview)
+            {
+                return false;
+            }
+            
+            bool isTextureNode = node.GetType().IsSubclassOf(typeof(TexturePropertyNode));
+            return isTextureNode ? ((TexturePropertyNode)node).IsValid : ReflectionUtils.GetPrivateField<bool>(node, "m_drawPreview");
+        }
+    }
+}
